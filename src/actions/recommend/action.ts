348d8f3f202961921b4b5ce00a691fb9cf96d1d8
Julian <juliangr@stud.ntnu.no>
2024-10-08 20:22:18 +0200

    fix: cleanup and add favicon

--- src/actions/recommend/action.ts
+++ src/actions/recommend/action.ts
@@ -9,70 +9,87 @@ import { google } from "@ai-sdk/google";
 import { generateObject } from "ai";
 import { Filter } from "bad-words";
 import { z } from "zod";
-import { unstable_noStore as noStore } from "next/cache";
 import sql from "@/db";
 
+/*
+ *
+ * queryAlreadyExists
+ *
+ * Server-side function to check if a query already exists in the database.
+ *
+ */
 async function queryAlreadyExists(query: string) {
-  // Check if query already exists in database
-
-  const res = await sql`
-        SELECT emoji_results 
-        FROM emoji_requests 
-        WHERE search_string = ${query};
-     `;
+  const res =
+    await sql` SELECT emoji_results FROM emoji_requests WHERE search_string = ${query};`;
 
   return res.rows;
 }
 
+/*
+ *
+ * cacheQuery
+ *
+ * Server-side function to cache a query and its results.
+ *
+ */
 async function cacheQuery(query: string, emojiResults: string) {
-  // Insert query into database
   await sql`SELECT upsert_emoji_request(${query}, ${emojiResults});`;
 }
 
+/*
+ *
+ * updateCount
+ *
+ * Server-side function to update the count and last_queried timestamp of a query.
+ *
+ */
+async function updateCount(query: string) {
+  await sql` UPDATE emoji_requests SET last_queried = CURRENT_TIMESTAMP, total_queries = total_queries + 1 WHERE search_string = ${query}; `;
+}
+
+/*
+ *
+ * generateEmojiRecommendations
+ *
+ * Server-side function to generate emoji recommendations based on a query.
+ *
+ */
 export async function generateEmojiRecommendations(
   _: z.infer<typeof recommendationInputSchema>,
   formData: FormData,
 ) {
   try {
-    noStore();
-    // Validate input
     const query = formData.get("query");
 
+    // Validate the input query
     const result = recommendationInputSchema.safeParse({ query });
-
     if (!result.success) throw new Error("Invalid input provided");
 
     const { query: temp_query } = result.data;
 
+    // Trim and lowercase the input query
     const sanitizedQuery = temp_query.trim().toLowerCase();
 
     const exists = await queryAlreadyExists(sanitizedQuery);
-    if (exists.length > 0) {
-      // console.log("retrieved result from database");
-      // cacheQuery(sanitizedQuery, exists[0].emoji_results);
-      // return { result: exists[0].emoji_results };
 
-      console.log("retrieved result from database");
-      // Optionally update last_queried and total_queries
-      await sql`
-        UPDATE emoji_requests
-        SET last_queried = CURRENT_TIMESTAMP,
-            total_queries = total_queries + 1
-        WHERE search_string = ${sanitizedQuery};
-      `;
+    if (exists.length > 0) {
+      console.log(`${sanitizedQuery} already exists in the database.`);
+
+      updateCount(sanitizedQuery);
       return { result: exists[0].emoji_results };
     }
 
-    // Filter out profanity
+    // Use a profanity filter to check for inappropriate language
     const filter = new Filter();
 
     if (filter.isProfane(sanitizedQuery)) {
       throw new Error("Please don't use profanity in your query 😇");
     }
 
+    // Get the AI configuration
     const { model, system, safetySettings } = config;
 
-    // Call AI service with validated query
+    // Generate the response object
     const { finishReason, object } = await generateObject({
       model: google(model, {
         safetySettings,
@@ -85,12 +102,12 @@ export async function generateEmojiRecommendations(
     // Handle rate-limiting or unexpected behavior
     if (finishReason !== "stop") throw new Error("Failed to generate object");
 
+    // Cache the query and its results
     await cacheQuery(sanitizedQuery, JSON.stringify(object.response));
 
-    // Return successful response
     return { result: object.response };
   } catch (e) {
-    // Handle errors and return consistent error structure
+    // Convert errors
     const error =
       e instanceof Error
         ? { message: e.message }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import ToastProvider from "@/providers/ToastProvider";
 import Footer from "@/components/layout/Footer";
 
 export const metadata: Metadata = {
-  title: "🤖 EmojAI - AI Emoji Recommender",
+  title: "EmojAI - AI Emoji Recommender",
   description:
     "Utilise the power of ✨AI ✨ to recommend emojis for your text!",
 };
@@ -17,6 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
+        />
+      </head>
       <body className={`antialiased prose min-h-screen`}>
         <ToastProvider>{children}</ToastProvider>
         <Footer />
